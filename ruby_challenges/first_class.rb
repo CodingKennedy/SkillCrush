d0a8ae2e5aa9935ffef3c88b6ffa6ce7713bc8c3
spiritninja66 <spiritninja66@aol.com>
2014-09-15 09:55:45 -0400

    Changed getter and setter methods to attr_accessor

--- ruby_challenges/first_class.rb
+++ ruby_challenges/first_class.rb
@@ -1,22 +1,11 @@
 class Album
-  def set_album=(track)
-    @track = track
-  end
-  def get_track
-    return @track
-  end
-  def set_artist=(artist)
-    @artist = artist
-  end
-  def get_artist
-    return @artist
-  end
+  attr_accessor :track, :artist
   def music_type
     return"#{@track} is written and played by #{@artist}."
   end
 end
 
 my_album = Album.new
-my_album.set_album= 'Little Machines'
-my_album.set_artist= 'Lights Bokan'
+my_album.track= 'Little Machines'
+my_album.artist= 'Lights Bokan'
 puts my_album.music_type
