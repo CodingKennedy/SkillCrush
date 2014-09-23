7bfed0a6fef2645cfc28d8f1bfa9c86e9cd06e8c
SpiritJinja66 <spiritninja66@aol.com>
2014-09-23 11:00:29 -0400

    Twilio app is still not working.

--- ruby_challenges/twilio.rb
+++ ruby_challenges/twilio.rb
@@ -1,21 +1,21 @@
-require 'rubygems' # not necessary with ruby 1.9 but included for completeness 
-require 'twilio-ruby' 
- 
-# put your own credentials here 
-account_sid = 'ACc736cb2bedf9e1d56970dfca39b99b77' 
-auth_token = '5623fd41155147879618ed75e3855e23' 
- 
-# set up a client to talk to the Twilio REST API 
-@client = Twilio::REST::Client.new account_sid, auth_token 
- 
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'ACc736cb2bedf9e1d56970dfca39b99b77'
+auth_token = '5623fd41155147879618ed75e3855e23'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
 message = @client.account.sms.messages.create(
-	:from => '+16466633004', 
-	:to => '+16469122743', 
-	:body => 'I am your magic 8 ball. Ask me anything!',  
+	:from => '+16466633004',
+	:to => '+16469122743',
+	:body => 'I am your magic 8 ball. Ask me anything!',
 )
 puts message.to
 
-
+gets
 
 answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Cannot predict now','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Concentrate and ask again','Dont count on it',' My reply is no','My sources say no','Yes definitely','Outlook not so good','Very doubtful']
 
