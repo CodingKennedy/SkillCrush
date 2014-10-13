3cf681c5c258caf6b76014265be6229246244ae0
SpiritJinja66 <spiritninja66@aol.com>
2014-10-12 20:59:16 -0400

    Update to twilio app.

--- ruby_challenges/twilio.rb
+++ ruby_challenges/twilio.rb
@@ -1,6 +1,15 @@
 require 'rubygems' # not necessary with ruby 1.9 but included for completeness
 require 'twilio-ruby'
 
+
+# puts message.to
+
+# gets
+
+answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Cannot predict now','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Concentrate and ask again','Dont count on it',' My reply is no','My sources say no','Yes definitely','Outlook not so good','Very doubtful']
+
+reply = answers[(0..19).to_a.sample]
+
 # put your own credentials here
 account_sid = 'ACc736cb2bedf9e1d56970dfca39b99b77'
 auth_token = '5623fd41155147879618ed75e3855e23'
@@ -11,13 +20,7 @@ auth_token = '5623fd41155147879618ed75e3855e23'
 message = @client.account.sms.messages.create(
 	:from => '+16466633004',
 	:to => '+16469122743',
-	:body => 'I am your magic 8 ball. Ask me anything!',
+	:body => reply,
 )
-puts message.to
 
-gets
-
-answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Cannot predict now','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Concentrate and ask again','Dont count on it',' My reply is no','My sources say no','Yes definitely','Outlook not so good','Very doubtful']
-
-puts answers[(0..19).to_a.sample]
 
